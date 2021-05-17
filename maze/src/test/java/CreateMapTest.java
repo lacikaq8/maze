98d64c741e171b003a6a70e1908f814c8d796331
unknown <lacikaq9@gmail.com>
2021-05-18 00:24:13 +0200

    CreateMapTest

---
+++ maze/src/test/java/CreateMapTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateMapTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void accLabPrime() {
+    }
+}
