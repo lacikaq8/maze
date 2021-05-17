30dc2ce2393cb2917b2ae716f76548805606cf42
unknown <lacikaq9@gmail.com>
2021-05-17 23:36:58 +0200

    app.java-ban az alap valtozok+tarolok

--- maze/src/main/java/app.java
+++ maze/src/main/java/app.java
@@ -36,6 +36,43 @@ class node {
  * A játékunk törzse
  */
 public class app extends Application {
-
+    /**
+     * a mapunk merete amit majd a prim algoritmus felhasznal a map genarashoz
+     */
+    public int Size = 15;// Effective map size, used for Prim algorithm to generate map
+    /**
+     * cellánk side hosszusaga
+     */
+    public static final int Range = 30;// cell side length
+    /**
+     * a mapunk igazi mérete
+     */
+    public int VSize = (Size * 2 + 1) * Range;// Actual map size
+    /**
+     * térképünk
+     */
+    public int maze[][] = new int[VSize][VSize];// map
+    /**
+     * a már látogatott utvonal
+     */
+    public int vis[][] = new int[VSize][VSize];// Visited path
+    /**
+     * uj nodot csinálunk
+     */
+    public node f[][] = new node[VSize][VSize];
+    /**
+     * mozgatási directionünk
+     */
+    public int[][] dir = { { -Range, 0 }, { Range, 0 }, { 0, -Range }, { 0, Range } };// moving direction
+    /**
+     * meghivjuk a createmap osztályt
+     */
+    public CreateMap c = new CreateMap(Size, Size);
+    /**
+     * létrehozunk egy téglalapot a rangek nagyságával
+     */
+    Rectangle rec = new Rectangle(Range, Range, Range, Range);
+    private int recX = 30, recY = 30;
+    private boolean autoPath = false;// Whether to enable automatic solution
 }
 
