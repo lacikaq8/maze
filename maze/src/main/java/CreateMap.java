3d90163259434b98fb48a5a26a90aa0d415e5cd4
unknown <lacikaq9@gmail.com>
2021-05-17 23:29:39 +0200

    createmap hozzáadva

--- maze/src/main/java/CreateMap.java
+++ maze/src/main/java/CreateMap.java
@@ -13,5 +13,18 @@ public class CreateMap {
     public int[][] map;
     private int r;
     private int c;
+    /**
+     * Megadjuk neki a oszlopokat és sorokat majd visszaad egy olyan méretú tömböt
+     * @param r0 vector sorok szamat keri
+     * @param c0 vector oszlopok szamat keri
+     */
+    CreateMap(int r0, int c0) {
+        row = r0;
+        column = c0;
+        r = 2 * row + 1;
+        c = 2 * column + 1;
+
+        map = new int[r][c];
+    }
 }
 
